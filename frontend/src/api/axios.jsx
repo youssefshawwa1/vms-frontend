6c0449ba5ef6af919b5b3558f20ccd99265958db
youssefshawwa1 <youssefshawwa1@gmail.com>
2026-01-02 21:22:43 +0200

    adding the new api link

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // Use the root URL so you can hit different endpoints
-  baseURL: "http://localhost:5000",
+  baseURL: "https://vms-backend-t90i.onrender.com",
 });
 
 // This interceptor sits between your app and the server
